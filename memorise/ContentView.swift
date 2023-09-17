61e10592889407d448cf6a9bc05e4d20be414e92
SpencerMckay <91818703+SpencerMckay@users.noreply.github.com>
2023-09-17 14:14:23 +1200

    added GeometryReader dynamic emoji sizing

--- memorise/ContentView.swift
+++ memorise/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
             Text("\(emojiMemoryGame.activeTheme.themeName)")
                 
                 ScrollView{
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 60))]) { // Why is this a lazy grid?
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum:75))]) { // Why is this a lazy grid?
                         ForEach(emojiMemoryGame.cards) {
                             card in CardView(card: card, cardColor: emojiMemoryGame.activeTheme.cardsColour)
                                 .aspectRatio(2/3, contentMode: .fit)
@@ -54,26 +54,37 @@ struct CardView: View {
     let cardColor: String
 
     var body: some View {
-        ZStack {
-            let shape = RoundedRectangle(cornerRadius: 20)
-            
-            if card.isFaceUp {
-                shape.fill().foregroundColor(.white)
-                shape.strokeBorder(lineWidth: 3)
-                Text(card.content)
-                    .font(.largeTitle)
-            }
-            else if card.isMatched {
-                shape.opacity(0)
-            }
-            else {
-                shape.fill()
-            }
+        GeometryReader(content: { geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRaidus)
                 
-        }
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                    Text(card.content)
+                        .font(font(in: geometry.size))
+                }
+                else if card.isMatched {
+                    shape.opacity(0)
+                }
+                else {
+                    shape.fill()
+                }
+            }
+        })
+
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.emojiScaler)
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRaidus: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let emojiScaler: CGFloat = 0.75
     }
 }
-    
 
 
 struct EmojiMemoryGame_Previews: PreviewProvider {
@@ -86,5 +97,3 @@ struct EmojiMemoryGame_Previews: PreviewProvider {
         
     }
 }
-    
-
